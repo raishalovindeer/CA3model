0f4362dd71fe1f7faac4a0688dc12dbe71b0a44a
Raisha Lovindeer <raisha.lovindeer@gmail.com>
2020-06-23 11:45:15 -0700

    clean conflicting code

--- dNvdt.m
+++ dNvdt.m
@@ -9,36 +9,24 @@ if Irradiance(130)>=Irradiance(230)
 else alpha = data.alpha_red;
 end
 photons_absorbed = [photons_absorbed;v*Ikfp(1,:)+(1-v)*Ikfp(2,:)+Ikfp(3,:)]; 
-<<<<<<< HEAD
-vdot = simpsons(Ikfp(1,:)-Ikfp(2,:),data.z(1),data.zm,data.nz-1)*(phi_fp*alpha/data.zm);
-% vdot = trapz(data.z,Ikfp(1,:)-Ikfp(2,:),2)*(phi_fp*alpha/data.zm);
-=======
-% vdot = simpsons(Ikfp(1,:)-Ikfp(2,:),data.z(1),data.zm,data.nz-1)...
-%         *(data.phi_fp*alpha/data.zm);
-vdot = trapz(data.z,Ikfp(1,:)-Ikfp(2,:),2)*(data.phi_fp*alpha/data.zm);
->>>>>>> simpsons
+vdot = simpsons(Ikfp(1,:)-Ikfp(2,:),data.z(1),data.zm,data.nz-1)...
+        *(data.phi_fp*alpha/data.zm);
+% vdot = trapz(data.z,Ikfp(1,:)-Ikfp(2,:),2)*(data.phi_fp*alpha/data.zm);
 if v>=1 
     vdot = 0; 
 elseif v<=0
     vdot = 0; 
 end
-<<<<<<< HEAD
-Ndot = simpsons(data.pmax*photons_absorbed./(data.pmax./[phi_gp;phi_rp;phi_fp]+photons_absorbed),data.z(1),data.zm,data.nz-1)*Nv(1:3,:)/data.zm-data.L*Nv(1:3,:);
-% Ndot = trapz(data.z,data.pmax*photons_absorbed./(data.pmax./[phi_gp;phi_rp;phi_fp]+photons_absorbed),2).*Nv(1:3,:)/data.zm-data.L*Nv(1:3,:);
-Nvdot = [Ndot;vdot];
-keyboard
-=======
-% N1i = simpsons(data.pmax*photons_absorbed(1,:)./...
-%     (data.pmax/data.phi_gp+photons_absorbed(1,:)),data.z(1),data.zm,data.nz);
-% N2i = simpsons(data.pmax*photons_absorbed(2,:)./...
-%     (data.pmax/data.phi_rp+photons_absorbed(2,:)),data.z(1),data.zm,data.nz);
-% N3i = simpsons(data.pmax*photons_absorbed(3,:)./...
-%     (data.pmax/data.phi_fp+photons_absorbed(3,:)),data.z(1),data.zm,data.nz);
-% Ndot = ([N1i;N2i;N3i].*Nv(1:3,:))/data.zm - data.L*Nv(1:3,:);
-Ndot = trapz(data.z,data.pmax*photons_absorbed./(data.pmax./...
-        [data.phi_gp;data.phi_rp;data.phi_fp]+photons_absorbed),2).*...
-        Nv(1:3,:)/data.zm-data.L*Nv(1:3,:);
+N1i = simpsons(data.pmax*photons_absorbed(1,:)./...
+    (data.pmax/data.phi_gp+photons_absorbed(1,:)),data.z(1),data.zm,data.nz);
+N2i = simpsons(data.pmax*photons_absorbed(2,:)./...
+    (data.pmax/data.phi_rp+photons_absorbed(2,:)),data.z(1),data.zm,data.nz);
+N3i = simpsons(data.pmax*photons_absorbed(3,:)./...
+    (data.pmax/data.phi_fp+photons_absorbed(3,:)),data.z(1),data.zm,data.nz);
+Ndot = ([N1i;N2i;N3i].*Nv(1:3,:))/data.zm - data.L*Nv(1:3,:);
+% Ndot = trapz(data.z,data.pmax*photons_absorbed./(data.pmax./...
+%         [data.phi_gp;data.phi_rp;data.phi_fp]+photons_absorbed),2).*...
+%         Nv(1:3,:)/data.zm-data.L*Nv(1:3,:);
 Nvdot = [Ndot;vdot];
 % keyboard
->>>>>>> simpsons
 end
